8b194186455239bd52ef957507d5d265a71c073a
Kazak <strannik19742@mail.ru>
2025-10-27 19:37:04 +0300

    ref

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 import './charList.scss';
 import MarvelService from '../../services/MarvelService';
-import Spinner from '../spinner/Spinner'
-import ErrorMessage from '../errorMessage/ErrorMessage'
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 import abyss from '../../resources/img/abyss.jpg';
 
 class CharList extends Component {
@@ -12,12 +12,13 @@ class CharList extends Component {
         chars: [],
         loading: true,
         error: false,
-        offset: 210,      // стартовое смещение Marvel API
-        newItemLoading: false, // для "load more"
+        offset: 210,
+        newItemLoading: false,
         charEnded: false
     };
 
     marvelService = new MarvelService();
+    itemsRefs = [];
 
     componentDidMount() {
         this.onRequest();
@@ -25,8 +26,8 @@ class CharList extends Component {
 
     onRequest = (offset) => {
         this.setState({ loading: true, error: false });
-
         this.onCharListLoading();
+
         this.marvelService
             .getAllCharacters(offset)
             .then(this.onCharsLoaded)
@@ -34,60 +35,72 @@ class CharList extends Component {
     }
 
     onCharListLoading = () => {
-        this.setState({
-            newItemLoading: true
-        })
+        this.setState({ newItemLoading: true });
     }
 
     onCharsLoaded = (newChars) => {
-        let ended = false
-        if (newChars.length < 9) { // Проверяем, есть ли новые персонажи
-            ended = true // если меньше лимита, список кончился
+        let ended = false;
+        if (newChars.length < 9) {
+            ended = true;
         }
 
-        this.setState(({ chars }) => ({
+        this.setState(({ chars, offset }) => ({
             chars: [...chars, ...newChars],
             loading: false,
             newItemLoading: false,
-            offset: this.state.offset + 9,
+            offset: offset + 9,
             charEnded: ended
         }));
     }
 
     onError = () => {
-        this.setState({
-            loading: false,
-            error: true
-        });
+        this.setState({ loading: false, error: true });
     }
 
+    // 🔹 Управление выделением и фокусом
+    focusOnItem = (i) => {
+        this.itemsRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemsRefs[i].classList.add('char__item_selected');
+        this.itemsRefs[i].focus();
+    };
+
     renderItems(arr) {
-        const items = arr.map(item => {
+        this.itemsRefs = [];
+
+        const items = arr.map((item, i) => {
             const imgStyle = item.imageAvailable ? { objectFit: 'cover' } : { objectFit: 'contain' };
 
             return (
-                <li 
-                className="char__item" 
-                key={item.id}
-                onClick={() => this.props.onCharSelected(item.id)}>
+                <li
+                    className="char__item"
+                    key={item.id}
+                    tabIndex={0}
+                    ref={(el) => (this.itemsRefs[i] = el)}
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}
+                    onKeyDown={(e) => {
+                        if (e.key === ' ' || e.key === 'Enter') {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}
+                >
                     <img src={item.thumbnail || abyss} alt={item.name} style={imgStyle}/>
                     <div className="char__name">{item.name}</div>
                 </li>
             );
         });
 
-        return (
-            <ul className="char__grid">
-                {items}
-            </ul>
-        );
+        return <ul className="char__grid">{items}</ul>;
     }
 
     render() {
         const { chars, loading, error, newItemLoading, offset, charEnded } = this.state;
 
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
         const content = !(loading || error) ? this.renderItems(chars) : null;
 
         return (
@@ -95,10 +108,10 @@ class CharList extends Component {
                 {errorMessage}
                 {spinner}
                 {content}
-                <button 
+                <button
                     className="button button__main button__long"
                     disabled={newItemLoading}
-                    style={{'display': charEnded ? 'none' : 'block'}}
+                    style={{ display: charEnded ? 'none' : 'block' }}
                     onClick={() => this.onRequest(offset)}
                 >
                     <div className="inner">load more</div>
@@ -108,12 +121,12 @@ class CharList extends Component {
     }
 }
 
-CharList.propTypes ={
+CharList.propTypes = {
     onCharSelected: PropTypes.func.isRequired
-}
+};
+
 CharList.defaultProps = {
     onCharSelected: () => {}
-}
-
+};
 
 export default CharList;
